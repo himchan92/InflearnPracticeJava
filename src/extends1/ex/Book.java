28cb5cb857c1353d172e7747527701bda5fd472d
omea92 <thdjun92@naver.com>
2025-02-22 12:27:50 +0900

    도서관리 상속, super 생성자 실습 반영

---
+++ src/extends1/ex/Book.java
@@ -0,0 +1,16 @@
+package extends1.ex;
+
+public class Book extends Item{
+    String author;
+    String isbn;
+
+    public Book(String name, int price, String author, String isbn) {
+        super(name, price); //부모생성자 호출, 첫줄에 명시 필수
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public void print() {
+        System.out.println(super.name + ", " + super.price + ", " + author + ", " + isbn);
+    }
+}
