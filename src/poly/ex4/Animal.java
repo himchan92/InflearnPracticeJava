72d8e6c9f13716bc993fa1e8b82e79d468e64f22
user <thdjun92@naver.com>
2025-02-23 15:02:01 +0900

    인터페이스 구현 실습 반영

---
+++ src/poly/ex4/Animal.java
@@ -0,0 +1,8 @@
+package poly.ex4;
+
+//인터페이스는 추상클래스 더 편리하게
+//메소드에 public abstract 내재되어있어 메소드 명시할때 생략가능(생략 권장)
+public interface Animal {
+    void sound();
+    void move();
+}
---
+++ src/poly/ex4/Dog.java
@@ -0,0 +1,13 @@
+package poly.ex4;
+
+public class Dog implements Animal{
+    @Override
+    public void sound() {
+        System.out.println("강아지 멍멍");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("강아지 달린다.");
+    }
+}
