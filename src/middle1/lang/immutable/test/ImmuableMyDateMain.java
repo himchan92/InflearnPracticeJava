eb554e3a1ad09fbf6714db8388061ff7ce7f53e4
himchan92 <omea92@naver.com>
2025-02-25 13:01:44 +0900

    불변객체 사용이유 주석 추가

--- src/middle1/lang/immutable/test/ImmuableMyDateMain.java
+++ src/middle1/lang/immutable/test/ImmuableMyDateMain.java
@@ -1,5 +1,11 @@
 package middle1.lang.immutable.test;
 
+/*
+    불변(final) 사용이유
+    - 개발자의 무분별한 값 변경 및 실수로 인한 변경 방지
+    - 인스턴스 생성통해서만 변경가능
+    - 기존 값이 변경되어도 영향안미침
+ */
 public class ImmuableMyDateMain {
     public static void main(String[] args) {
         ImmuableMyDate immuableMyDate = new ImmuableMyDate(2025, 2, 25);
