ea158850a25d1c92378ee49c71954b5eee81a711
omea92 <thdjun92@naver.com>
2025-02-25 22:58:25 +0900

    StringBuilder 예제 실습2

--- src/middle1/lang/string/builder/StringBuilderMain1_1.java
+++ src/middle1/lang/string/builder/StringBuilderMain1_1.java
@@ -3,7 +3,14 @@ package middle1.lang.string.builder;
 public class StringBuilderMain1_1 {
 
     public static void main(String[] args) {
-        // StringBuilder : 가변으로 변경 가능하며 String 처럼 선언할때마다 메모리 할당되지않아 메모리 절약, 성능향상(문자열 활용 로직 시 권장)
+        /*
+            StringBuilder
+            - 가변으로 변경 가능하며 String 처럼 선언할때마다 메모리 할당되지않아 메모리 절약, 성능향상(문자열 활용 로직 시 권장)
+            - 대용량 문자열 조작 작업 시 용이
+            - 단순한 문자열은 String 사용 무방
+            - 조건문, 반복문 통해 동적으로 문자열 조합, 변경 시 용이
+            - StringBuffer 는 동기화기능있어 멀티쓰레드에서 용이하나 잘안쓰고 Builder 자주 사용
+         */
         StringBuilder sb = new StringBuilder();
         sb.append("A");
         sb.append("B");
