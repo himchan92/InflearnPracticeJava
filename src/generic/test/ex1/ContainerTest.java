ac6331ea5bcb8fe7eb36369c7eeea16fcd45b30f
himchan92 <omea92@naver.com>
2025-03-19 12:27:50 +0900

    제네릭 타입 상속 동물 예제 실습

---
+++ src/generic/test/ex1/ContainerTest.java
@@ -0,0 +1,8 @@
+package generic.test.ex1;
+
+public class ContainerTest {
+    public static void main(String[] args) {
+        Container<String> stringContainer = new Container<>();
+        System.out.println(stringContainer.isEmpty());
+    }
+}
