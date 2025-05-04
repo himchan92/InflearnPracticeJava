ead7dfe724aec03d7943c89f72496ea67b42f6f2
omea92 <thdjun92@naver.com>
2025-05-05 01:11:29 +0900

    변수 예제문제 풀이 반영

---
+++ src/variable/ex/VarEx3Question.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3Question {
+
+    public static void main(String[] args) {
+        long num = 100000000000L;
+        System.out.println(num);
+        boolean flag = true;
+        System.out.println(flag);
+    }
+}
