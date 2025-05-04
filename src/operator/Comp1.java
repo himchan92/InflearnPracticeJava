d412ac9b7a87195bbb3c942322d3748149f3da0b
omea92 <thdjun92@naver.com>
2025-05-05 01:53:39 +0900

    연산자 개념 실습

---
+++ src/operator/Comp1.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class Comp1 {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+
+        System.out.println(a == b); //같은지
+        System.out.println(a != b); //다른지
+        System.out.println(a > b); //a가 큰지
+        System.out.println(a < b); //b가 큰지
+        System.out.println(a >= b); //a가 같거나 큰지
+        System.out.println(a <= b); //b가 같거나 큰지
+
+    }
+}
