175a09c2d3bc86dfbb9c6574be799fc4cb8937d9
user <thdjun92@naver.com>
2025-05-25 14:51:17 +0900

    세션5. 조건문 예제 실습

---
+++ src/cond/Switch1.java
@@ -0,0 +1,28 @@
+package cond;
+
+public class Switch1 {
+    public static void main(String[] args) {
+        int grade = 2;
+
+        int coupon;
+        switch(grade) {
+            case 1:
+                coupon = 1000;
+                break;
+
+            case 2:
+                coupon = 2000;
+                break;
+
+            case 3:
+                coupon = 3000;
+                break;
+
+            default:
+                coupon = 4000;
+                break;
+        }
+
+        System.out.println("발급쿠폰 : " + coupon);
+    }
+}
