9310dc0e72d4b453030eed959dfc5adab23b046e
omea92 <thdjun92@naver.com>
2025-08-09 12:47:03 +0900

    입문 - while문 예제

---
+++ src/loop/ex/WhileEx2.java
@@ -0,0 +1,12 @@
+package loop.ex;
+
+public class WhileEx2 {
+
+  public static void main(String[] args) {
+    for(int i = 2; i < 20; i++) {
+      if(i % 2 == 0) {
+        System.out.println(i);
+      }
+    }
+  }
+}
