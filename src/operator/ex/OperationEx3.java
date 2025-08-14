3ac6a9149590bbae5685f2b575c1062fe8274f67
himchan92 <omea92@naver.com>
2025-08-14 11:59:41 +0900

    init commit

--- src/operator/ex/OperationEx3.java
+++
@@ -1,9 +0,0 @@
-package operator.ex;
-
-public class OperationEx3 {
-    public static void main(String[] args) {
-        int score = 90;
-
-        System.out.println((score >= 80 && score <= 100) ? true : false);
-    }
-}
