c5396b7fbd7ec696a324fc572ccc58f4e32ae89d
omea92 <thdjun92@naver.com>
2025-08-15 12:08:08 +0900

    반복문 복습

---
+++ src/loop/ex/LoopEx1.java
@@ -0,0 +1,13 @@
+package loop.ex;
+
+public class LoopEx1 {
+
+  public static void main(String[] args) {
+    int num = 2;
+    for(int i = 0; i < 10; i++) {
+      System.out.println(num + i);
+      num++;
+    }
+  }
+
+}
