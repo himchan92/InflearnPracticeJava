011e8e81dd56980e4ec249540e06bc7f18775519
omea92 <thdjun92@naver.com>
2025-09-15 20:07:38 +0900

    복습

---
+++ src/variable/Var5.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+        int a; //변수선언
+        a = 1;
+        System.out.println(a);
+
+        int b = 2; //선언 동시에 초기화 가능
+        System.out.println(b);
+
+        int c = 3, d = 4;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
