2c9dc3db9292c038f0150786a6e6bf6e04d6d089
omea92 <thdjun92@naver.com>
2025-09-15 20:42:47 +0900

    init

--- src/HelloJava2.java
+++
@@ -1,7 +0,0 @@
-public class HelloJava2 {
-    public static void main(String[] args) {
-        System.out.println("hello java");
-        System.out.println("hello java");
-        System.out.println("hello java");
-    }
-}
--- src/variable/Var6.java
+++
@@ -1,8 +0,0 @@
-package variable;
-
-public class Var6 {
-    public static void main(String[] args) {
-        int a;
-        //System.out.println(a); //할당안하면 에러
-    }
-}
