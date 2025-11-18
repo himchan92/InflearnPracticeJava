b96acf26a0879005f05e08d23f976c63d1e31296
himchan92 <omea92@naver.com>
2025-11-18 12:32:56 +0900

    입문 - 변수/연산자실습

---
+++ src/variable/Var5.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+        int a;
+        a = 1;
+        System.out.println(a);
+    }
+}
---
+++ src/variable/ex/OperationEx3.java
@@ -0,0 +1,9 @@
+package variable.ex;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 77;
+
+        System.out.println((score >= 80 && score <= 100) ? true : false);
+    }
+}
