1fec712e06c6f5d85d28f67b000d5fef689a012e
himchan92 <omea92@naver.com>
2026-02-27 14:10:14 +0900

    operator

---
+++ src/operator/ex/OperationEx3.java
@@ -0,0 +1,9 @@
+package operator.ex;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 94;
+        boolean flag = score >= 80 && score <= 100;
+        System.out.println(flag);
+    }
+}
