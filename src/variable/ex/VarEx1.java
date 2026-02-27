fbd2a0b5740d998aeafbd8ee2524a4fdbb58e183
himchan92 <omea92@naver.com>
2026-02-27 13:34:41 +0900

    review

---
+++ src/variable/ex/VarEx1.java
@@ -0,0 +1,13 @@
+package variable.ex;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        int num1 = 31;
+        int num2 = 42;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 % num2);
+    }
+}
